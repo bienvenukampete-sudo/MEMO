6f15daff5d1acd6f51b69c90d4f6b701b7458105
jordanbatanga <jordanbatanga@gmail.com>
2026-01-23 14:35:31 +0100

    Création de la classe Task

--- app/src/main/java/com/example/memo/Task.java
+++ app/src/main/java/com/example/memo/Task.java
@@ -1,1 +1,47 @@
+package com.example.memo;
 
+public class Task {
+    private int id;
+    private String title;
+    private Priority priority;
+    private boolean completed;
+
+    public Task(int id, String title, Priority priority, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.priority = priority;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
